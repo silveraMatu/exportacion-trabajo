20da086dea8893d7ed20bed141bb3cc12969bece
Matias Silvera <matusilvera808@gmail.com>
2025-06-06 11:04:36 -0300

    Trabajo de exportacion e importacion de modulos

---
+++ app.mjs
@@ -0,0 +1,7 @@
+import restar from "./exportacion-defecto.mjs";
+import { dividir, multiplicar, sumar } from "./exportacion-nombrada.mjs";
+
+console.log(restar(5, 4));
+console.log(multiplicar(5, 4));
+console.log(sumar(5, 4));
+console.log(dividir(5, 4));
