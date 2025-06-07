c262762585f4e49ce4cf04036efbdb229b5b980d
Matias Silvera <matusilvera808@gmail.com>
2025-06-06 22:18:46 -0300

    Consigna 1 y 2 de la clase terminadas.

--- cjs/app.cjs
+++
@@ -1,7 +0,0 @@
-const restar = require('./exportacion-defecto.cjs')
-const operaciones = require('./exportacion-nombrada.cjs');
-
-console.log(restar(5, 4))
-console.log(operaciones.sumar(5, 4))
-console.log(operaciones.multiplicar(5, 4))
-console.log(operaciones.dividir(5, 4))
--- mjs,/exportacion-defecto.mjs
+++
@@ -1,3 +0,0 @@
-export default function restar(a, b){
-    return a - b;
-}
